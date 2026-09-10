68730e71166102ddcfc7affc006cd6adb875b379
SenixArt <105945599+SenixArt@users.noreply.github.com>
2026-09-10 15:36:15 -0400

    Update PersonQueue.cs

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -1,35 +1,37 @@
-/// <summary>
-/// A basic implementation of a Queue
-/// </summary>
-public class PersonQueue
+using System.Collections.Generic;
+
+namespace week02.code
 {
-    private readonly List<Person> _queue = new();
-
-    public int Length => _queue.Count;
-
-    /// <summary>
-    /// Add a person to the queue
-    /// </summary>
-    /// <param name="person">The person to add</param>
-    public void Enqueue(Person person)
+    public class PersonQueue
     {
-        _queue.Insert(0, person);
-    }
+        private readonly List<Person> _queue = new();
 
-    public Person Dequeue()
-    {
-        var person = _queue[0];
-        _queue.RemoveAt(0);
-        return person;
-    }
+        public int Length => _queue.Count;
 
-    public bool IsEmpty()
-    {
-        return Length == 0;
-    }
+        /// <summary>
+        /// Add a person to the queue
+        /// </summary>
+        /// <param name="person">The person to add</param>
+        public void Enqueue(Person person)
+        {
+            _queue.Add(person);
+        }
 
-    public override string ToString()
-    {
-        return $"[{string.Join(", ", _queue)}]";
+        public Person Dequeue()
+        {
+            var person = _queue[0];
+            _queue.RemoveAt(0);
+            return person;
+        }
+
+        public bool IsEmpty()
+        {
+            return Length == 0;
+        }
+
+        public override string ToString()
+        {
+            return $"[{string.Join(", ", _queue)}]";
+        }
     }
 }
